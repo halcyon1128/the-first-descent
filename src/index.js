feba5ea9a35756bc92ec7e63ecf45e2fbda828fc
halcyon1128 <acgorospeofficial@gmail.com>
2025-05-08 22:52:16 +0800

    v1.5.2 update for gitpages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// src/index.js
-import { h, render } from 'preact'
+import { render } from 'preact'
 import App from './App'
-import '../tailwind.css'
+import '../tailwind.css' // Ensure this path is correct
 
 render(<App />, document.getElementById('app'))
